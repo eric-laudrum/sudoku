95155ec2f21d227d57eadb8d758192b5045976b8
eric-laudrum <ericlaudrum@gmail.com>
2025-12-14 00:29:20 -0500

    Generate grid in MainActivity and create cell_border drawable

--- app/src/main/java/com/example/sudoku/MainActivity.kt
+++ app/src/main/java/com/example/sudoku/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.sudoku
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.GridLayout
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    // ---------- Variables ----------
+    private lateinit var gridLayout: GridLayout
+    private lateinit var cellViews: Array<Array<TextView>>
+
+    // ---------- Create ----------
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        gridLayout = findViewById(R.id.grid)
+
+        gridLayout.post{
+            createBoard()
+        }
+    }
+
+    // ---------- Functions ----------
+    private fun createBoard() {
+        val boardSize = 9
+        cellViews = Array(boardSize){ row ->
+            Array(boardSize) {col ->
+                TextView(this).apply{
+                    layoutParams = GridLayout.LayoutParams().apply{
+                        width = gridLayout.width / boardSize
+                        height = gridLayout.height / boardSize
+                        rowSpec = GridLayout.spec(row)
+                        columnSpec = GridLayout.spec(col)
+                    }
+
+                    gravity = Gravity.CENTER
+                    textSize = 20f
+                    setBackgroundResource(R.drawable.cell_border)
+
+                }
+            }
+        }
+
+
+        // Add TextViews to Grid
+        for(row in 0 until boardSize){
+            for(col in 0 until boardSize){
+                gridLayout.addView(cellViews[row][col])
+            }
         }
     }
 }
+
+
